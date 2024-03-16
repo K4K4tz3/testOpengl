d8ea15cbc1a78487290f495e816f4bbfbd54bc45
K4K4tz3 <88493804+K4K4tz3@users.noreply.github.com>
2024-03-17 00:37:14 +0100

    added Element Buffer Objects

--- main.cpp
+++ main.cpp
@@ -22,11 +22,16 @@ const char *fragmentShaderSource = "#version 330 core\n"
                                    "}\n\0";
 
 float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, // top left
+        0.5f, 0.5f, 0.0f, // top right
+        0.5f, -0.5f, 0.0f // bottom right
 };
-unsigned int VBO, VAO;
+unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+};
+unsigned int VBO, VAO, EBO;
 unsigned int vertexShader;
 unsigned int fragmentShader;
 unsigned int shaderProgram;
@@ -101,13 +106,17 @@ int main(){
     // Buffer Setup
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     // bind vertex array
     glBindVertexArray(VAO);
     // copy vertice arrays into buffer
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // set attributes
+    // copy index array in element buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // set vertex attribute pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -128,7 +137,8 @@ int main(){
         // draw first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
 
         // check events and swap buffers
