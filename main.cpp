49b45bb709778ae5aeef4c32b3fa0a402eaf505a
K4K4tz3 <88493804+K4K4tz3@users.noreply.github.com>
2024-03-16 22:47:20 +0100

    Basic Window Setup

--- main.cpp
+++ main.cpp
@@ -1,17 +1,65 @@
 #include <iostream>
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main() {
-    if (glfwInit() == GLFW_FALSE)
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// Settings
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
+
+int main(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Learn OpenGL", NULL, NULL);
+    if(window == NULL)
     {
-        return 0;
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Glad
+    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
     }
 
-    if (gladLoadGL() == 0)
+    // Viewport
+    glViewport(0, 0, SCR_HEIGHT, SCR_WIDTH);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Render Loop
+    while(!glfwWindowShouldClose(window))
     {
-        return 0;
+        // input
+        processInput(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check events and swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
+
+    // Terminate
+    glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
